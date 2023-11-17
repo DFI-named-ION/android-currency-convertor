4c49e0d03e402f47d78c1219f056fa759034c7c4
DFI-named-ION <larityandselnter@gmail.com>
2023-11-17 09:24:13 -0500

    init + hud?

---
+++ app/src/main/java/com/example/currencyconvertorapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.currencyconvertorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText valueTop = findViewById(R.id.value_top);
+    }
+}
